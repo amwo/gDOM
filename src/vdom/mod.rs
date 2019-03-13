c3ea3b58f1e865460e0f39bf7733c20476e0d655
amwo <nuude.ad@gmail.com>
2019-03-13 17:33:26 +0900

    Init impl structures.

--- src/vdom/mod.rs
+++ src/vdom/mod.rs
@@ -1,1 +1,37 @@
+macro_rules! render {
+    ($html:expr) => {
+        println!("{}", $html);
+    };
+}
 
+struct Vdom();
+
+impl Vdom {
+    pub fn init() -> {
+    }
+
+    pub fn get_before_state -> {
+    }
+
+    pub fn get_after_state -> {
+    }
+
+    pub fn diff() -> {
+    }
+
+    pub fn patch() -> {
+    }
+}
+
+#[test]
+fn test_render() {
+    render!(
+        r#"
+            <div>
+                <h1></h1>
+                <div>hoge</div>
+                <button>Click</button>
+            </div>
+        "#
+    );
+}
